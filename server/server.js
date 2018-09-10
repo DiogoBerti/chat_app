bd3c12026d54308fa6a6471699a830023cbb3d7d
DiogoBerti <diogoberti88@gmail.com>
2018-09-09 21:26:52 -0300

    [ADD] - Adicionados eventos e imports do socket io.. [ADD] - Primeiro evento do socket no front e no backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
+const socketIO = require('socket.io');
+const http = require('http');
 // Biblioteca que faz o gerenciamento do paths do sistema
 const path = require('path');
 
 const express = require('express');
 var app = express();
+// Cria um server http e usa o app express como argumento no lugar do (req, res)
+var server = http.createServer(app);
+// Traz o socket para dentro do server
+var io = socketIO(server);
+
 // usando o join, podemos juntar os valores para chegar nos locais...
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 // Setando o public para o express
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Checa se houve conexão pelo socket
+io.on('connection', (socket) =>{
+	console.log('new user connected');
+	// Checa se o usuario se desconectou da pagina (verificar script do front)
+	socket.on('disconnect', () =>{
+		console.log('User Disconnected');
+	});
+});
+
+
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
